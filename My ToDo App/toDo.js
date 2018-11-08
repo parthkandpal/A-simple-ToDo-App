db09ad8ba216e1ae9f68e29895246251d4e38c58
Parth Kandpal <42544546+parthkandpal@users.noreply.github.com>
2018-11-08 18:29:28 +0530

    Add files via upload

---
+++ My ToDo App/toDo.js
@@ -0,0 +1,74 @@
+// var h = document.createElement('h1')
+// var myValue= document.createTextNode('Hello World')
+
+// h.appendChild(myValue)
+// document.querySelector('h1').appendChild(h)
+// var num = 5
+// while (num >0){
+//     console.log(num)
+//     num--
+// }
+
+
+var ul=document.getElementById('list')
+var li; 
+
+var addButton = document.getElementById('add')
+addButton.addEventListener('click',addItem)
+
+var removeButton = document.getElementById('remove')
+
+removeButton.addEventListener('click',removeItem)
+
+
+
+function addItem(){
+    var input = document.getElementById('input');
+    var item = input.value;
+    ul = document.getElementById('list');
+    var textnode = document.createTextNode(item);
+
+    if (item === ''){
+        // var p = document.createElement('p')
+        // var text= document.createTextNode('ToDo item can\'t be balnk')
+        // p.appendChild(text)
+        alert('ToDo item can\'t be blank')
+        // return false;
+    } else{
+        //create li
+        li = document.createElement('li');
+
+        //create checkbox
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.setAttribute('id','check');
+
+        //create label
+        var label = document.createElement('label');
+        // label.setAttribute('for','item')
+
+
+        //add elements on the page
+        ul.appendChild(label)
+        li.appendChild(checkbox)
+        label.appendChild(textnode)
+        li.appendChild(label)
+        ul.insertBefore(li, ul.childNodes[0])
+        setTimeout(()=>{
+            li.className= 'visual'
+        },2)
+
+        input.value = ''
+
+    }
+}
+
+
+function removeItem(){
+    li = ul.children
+    for (let index = 0; index < li.length; index++) {
+        while (li[index] && li[index].children[0].checked){
+            ul.removeChild(li[index])
+        }
+    }
+}
